737006819dd26e24ff53840dedfa3fa4d00bc440
robots5771 <123756042+robots5771@users.noreply.github.com>
2026-06-03 17:49:15 +0100

    Update 1.5

--- Blackjack.py
+++ Blackjack.py
@@ -3,7 +3,7 @@ from Cards import Deck
 from Dealer import Dealer
 import random as rand
 
-class Blackjack (object):
+class Blackjack(object):
     def __init__ (self):
         self.player1 = Player()
         self.BlackjackDeck = Deck()
@@ -17,12 +17,12 @@ class Blackjack (object):
             self.Dealer1.Add_Cards(DealerCard)
             Tally += 1
 
-    def PlayerHitCards (self):
+    def PlayerHitCards(self):
         print(f"Player : {self.player1.Current_Hand()}")
         print(f"Player : {self.Dealer1.Current_Hand()}")
         
         Disable = 0
-        while Disable < 1 and self.player1.playercards != 21:
+        while Disable < 1 and self.player1.Sum_Card() != 21 and self.player1.Sum_Card() < 22:
             AskPlayer = input("'hit' or 'stand'\n")
             if AskPlayer == "hit":
                 PlayerCard = rand.choice(self.BlackjackDeck.cards)
@@ -33,15 +33,34 @@ class Blackjack (object):
                 Disable += 1
             
     def DealersTurn (self):
-        if self.player1.Sum_Card() < 22:
-            while self.Dealer1.Sum_Card() < 17:
+        while self.Dealer1.Sum_Card() < 17:
+            if self.player1.Sum_Card() < 22:
                 DealerCard = rand.choice(self.BlackjackDeck.cards)
                 self.Dealer1.Add_Cards(DealerCard)
                 if self.Dealer1.Sum_Card() > 17 or self.Dealer1.Sum_Card() > 21:  #This skips printing until the final dealer card is drawn to prevent spamming
                     print(self.player1.Current_Hand())
                     print(self.Dealer1.Current_Hand())
+                else:
+                    print(self.player1.Current_Hand())
+                    print(self.Dealer1.Current_Hand())
+
+
+    def WinLoseBust(self):
+        if self.player1.Sum_Card() > 21:
+            print("Bust")
+        if self.player1.Sum_Card() > self.Dealer1.Sum_Card():
+            print("Win")
+        if self.player1.Sum_Card() == 21 and self.Dealer1.Sum_Card() < 21:
+            print("Win")
+        if self.player1.Sum_Card() == 21 and self.Dealer1.Sum_Card() > 21:
+            print("Win")
+        if self.player1.Sum_Card() == self.Dealer1.Sum_Card():
+            print("Push")
+        else:
+            print("Lose")
             
     
 rungame = Blackjack()
 rungame.PlayerHitCards()
 rungame.DealersTurn()
+rungame.WinLoseBust()
